e5c97de287530159b0f14be79a5811796ed55b3e
LuisRoft <luisvelasco6541@gmail.com>
2025-02-03 17:25:57 -0500

    Texto to en - HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,15 @@ import { LucideTractor, LucideTruck, LucideWrench } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-[calc(100vh-4rem)] bg-neutral-900 px-4">
+    <div className="flex flex-col py-20 items-center justify-center min-h-[calc(100vh-4rem)] bg-neutral-900 px-4">
       <div className="max-w-3xl text-center space-y-8">
         <h1 className="text-5xl md:text-6xl font-bold tracking-tight text-white">
-          Partes y Repuestos para Maquinaria Industrial
+          Parts and Accessories for Industrial Machinery
         </h1>
 
         <p className="text-lg md:text-xl text-gray-300">
-          Encuentra los mejores componentes para tus equipos, desde piezas de
-          tractor hasta correas para cortadoras de césped. Calidad garantizada y
-          entrega rápida.
+          Find the best components for your equipment, from tractor parts to
+          lawn mower belts. Guaranteed quality and fast delivery.
         </p>
 
         <div className="flex justify-center">
@@ -21,7 +20,7 @@ export default function Home() {
             asChild
             className="bg-blue-500 hover:bg-blue-600 text-md px-8 py-4 rounded-lg transition-colors"
           >
-            <Link href="/tractor-parts">Ver Catálogo</Link>
+            <Link href="/tractor-parts">View Catalog</Link>
           </Button>
         </div>
 
@@ -31,31 +30,29 @@ export default function Home() {
               <LucideTractor className="text-blue-400" size={35} />
             </div>
             <h3 className="text-blue-400 text-xl font-semibold">
-              Partes de Tractor
+              Tractor Parts
             </h3>
             <p className="text-sm text-gray-400">
               John Deere, Caterpillar, Case
             </p>
           </div>
 
-          <div className="py-6 px-4 border border-neutral-800 rounded-lg flex flex-col items-center  hover:bg-gray-800 transition-colors">
-            <div className="bg-blue-200 rounded-xl p-1 mb-2 ">
+          <div className="py-6 px-4 border border-neutral-800 rounded-lg flex flex-col items-center hover:bg-gray-800 transition-colors">
+            <div className="bg-blue-200 rounded-xl p-1 mb-2">
               <LucideTruck className="text-blue-400" size={35} />
             </div>
-            <h3 className="text-blue-400 text-xl font-semibold">
-              Partes de Camiones
-            </h3>
+            <h3 className="text-blue-400 text-xl font-semibold">Truck Parts</h3>
             <p className="text-sm text-gray-400">
-              Repuestos originales y alternativos
+              Original and alternative spare parts
             </p>
           </div>
 
-          <div className="p-6 border border-neutral-800 rounded-lg flex flex-col items-center  hover:bg-gray-800 transition-colors">
+          <div className="p-6 border border-neutral-800 rounded-lg flex flex-col items-center hover:bg-gray-800 transition-colors">
             <div className="bg-blue-200 rounded-xl p-1 mb-2">
               <LucideWrench className="text-blue-400" size={35} />
             </div>
-            <h3 className="text-blue-400 text-xl font-semibold">Accesorios</h3>
-            <p className="text-sm text-gray-400">Neumáticos, Correas y más</p>
+            <h3 className="text-blue-400 text-xl font-semibold">Accessories</h3>
+            <p className="text-sm text-gray-400">Tires, Belts, and more</p>
           </div>
         </div>
       </div>
